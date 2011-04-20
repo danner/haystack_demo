0ac621844da34e29434b8c9abd0aad69da2ba504
Danner Claflin <danner@custommade.com>
2011-04-19 21:40:46 -0400

    presentation addons

--- README.txt
+++ README.txt
@@ -1,23 +1,25 @@
 Haystack Demo @ CustomMade.com
 
+wireless: nitrox36
+
 What?
 Why?
 
 Possible Topics:
     Install
     Facets
-    Advanced Views
     Data Preparation
     Advanced Views
     Scaling
 
 Links:
     http://haystacksearch.org
-
-
     Solr
         http://wiki.apache.org/solr/SolrTomcat
         http://www.tc.umn.edu/~brams006/solr_ubuntu.html
 
 Shell Commands Used:
+rm schema.xml && ./manage.py build_solr_schema >> schema.xml
+sudo cp schema.xml /usr/share/tomcat6/demo/conf/
+time python manage.py rebuild_index
 
--- school/search_indexes.py
+++ school/search_indexes.py
@@ -6,8 +6,18 @@ class MemberIndex(SearchIndex):
     text = CharField(document=True, use_template=True, template_name='indexes/member_text.txt')
     gpa = FloatField(model_attr="gpa", null=True)
     school = CharField(faceted=True, model_attr="school__name")
+    boost = FloatField()
 
     def get_queryset(self):
         return Member.objects.all()
+        
+    def prepare_boost(self, object):
+        boost = 1.0
+        if object.id == 45:
+            boost *= 5
+        return boost
+
+class SchoolIndex(SearchIndex):
+    text
 
 site.register(Member, MemberIndex)
